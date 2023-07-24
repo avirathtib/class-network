f7984f8a1c0202723fef95a982cd7137de2143d4
Mehul <mehulrastogi2002@gmail.com>
2023-07-23 21:43:45 -0700

    improved naming convention and cleaned up code

--- datacleaning.js
+++ datacleaning.js
@@ -2,30 +2,37 @@ const fs = require('fs');
 
 async function readFileAsync(inputFile) {
   try {
-    returnDictionary = {}
+    const returnDictionary = {};
+
     const data = await fs.promises.readFile(inputFile, 'utf8');
     const jsonData = JSON.parse(data);
-    const coursesinit = jsonData['courses']
-    for (const initkey in coursesinit) {
-        course = initkey
-        const listclass = coursesinit[initkey][1]
-        for (const key in listclass) {
-            const valueArray = listclass[key];
-            const sec = key
-            const crn = valueArray[0]
-            let time;
-            let day;
-            if (valueArray[1]?.[0]?.[0] !== undefined) {
-                time = valueArray[1][0][0];
-            }
-            if (valueArray[1]?.[0]?.[1] !== undefined) {
-                day = valueArray[1][0][1];
-            }
-            returnDictionary[crn] = [course, time, day]
-            console.log(course, sec, crn, time, day)
+    const coursesInit = jsonData['courses'];
+
+    for (const courseKey in coursesInit) {
+      const course = courseKey;
+      const listClass = coursesInit[courseKey][1];
+
+      for (const secKey in listClass) {
+        const sec = secKey; 
+        const valueArray = listClass[secKey];
+
+        const crn = valueArray[0];
+        let time;
+        let day;
+
+        if (valueArray[1]?.[0]?.[0] !== undefined) {
+          time = valueArray[1][0][0];
         }
+        if (valueArray[1]?.[0]?.[1] !== undefined) {
+          day = valueArray[1][0][1];
+        }
+
+        returnDictionary[crn] = [course, time, day];
+        console.log(course, sec, crn, time, day);
+      }
     }
-    console.log(returnDictionary)
+
+    console.log(returnDictionary);
   } catch (err) {
     console.error('Error reading or parsing the file:', err);
   }
