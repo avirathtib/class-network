9023617cd4ba3faa3e48bc4b50be3d8be5098112
Mehul <mehulrastogi2002@gmail.com>
2023-07-23 21:38:10 -0700

    entire cleaning works

--- datacleaning.js
+++ datacleaning.js
@@ -2,25 +2,30 @@ const fs = require('fs');
 
 async function readFileAsync(inputFile) {
   try {
-    // Asynchronously read the file
+    returnDictionary = {}
     const data = await fs.promises.readFile(inputFile, 'utf8');
     const jsonData = JSON.parse(data);
-    const course = jsonData['courses']['ACCT 2101'];
-    const listclass = course[1]
-    for (const key in listclass) {
-        // If you want to access individual elements within the array value
-        const valueArray = listclass[key];
-        const sec = key
-        const crn = valueArray[0]
-        const time = valueArray[1][0][0]
-        const day = valueArray[1][0][1]
-        console.log("new clas")
-        console.log(sec)
-        console.log(crn)
-        console.log(time)
-        console.log(day)
-      }
-
+    const coursesinit = jsonData['courses']
+    for (const initkey in coursesinit) {
+        course = initkey
+        const listclass = coursesinit[initkey][1]
+        for (const key in listclass) {
+            const valueArray = listclass[key];
+            const sec = key
+            const crn = valueArray[0]
+            let time;
+            let day;
+            if (valueArray[1]?.[0]?.[0] !== undefined) {
+                time = valueArray[1][0][0];
+            }
+            if (valueArray[1]?.[0]?.[1] !== undefined) {
+                day = valueArray[1][0][1];
+            }
+            returnDictionary[crn] = [course, time, day]
+            console.log(course, sec, crn, time, day)
+        }
+    }
+    console.log(returnDictionary)
   } catch (err) {
     console.error('Error reading or parsing the file:', err);
   }
